221811af74d85444a24ff8eef0bbb8ceefaa5a84
Sergey Kluev <reaswer@gmail.com>
2012-05-12 11:35:48 +0400

    12.05.12

--- task.c
+++ task.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+#define MAXBUFFERSIZE 80
+
 int main()
 {
-	//Code will be here soon =)
+	char buffer[MAXBUFFERSIZE];	/* Буффер для команды */
+	char c;
+	int count;			/* Счетчик символов */
+	int exit_flag = 1;
+
+	/*Считываем команды*/
+	while(exit_flag)
+	{		
+		printf("Enter a new command...\n"
+			"Type '0' in the end of the command\n");
+		c = getchar();
+		count = 0;
+
+		while((c != '0') && (count < MAXBUFFERSIZE))
+		{
+			buffer[count++] = c;
+			c = getchar();
+
+		}
+		
+		buffer[count] = '\0';
+		printf("The command u entered was:%s\n", buffer);
+		exit_flag = 0;	
+	}
 }
