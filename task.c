dd4d4706c9b94498fb07093f5e7c25f6b102eeda
Sergey Kluev <reaswer@gmail.com>
2012-05-13 17:58:48 +0400

    13.05.12

--- task.c
+++ task.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAXBUFFERSIZE 80
+
+/* Функция выделяет первое слово, остальные слова помещает в массив.
+   На основании выделенного слова вызывается соответсвующая функция,
+   если такой нет выдаётся ошибка */
+void parsing_command(char *command);
+void add(char *p[], int number);
 
 int main()
 {
@@ -12,6 +20,8 @@ int main()
 	/*Считываем команды*/
 	while(exit_flag)
 	{		
+		char *buf = (char*)malloc(MAXBUFFERSIZE);
+
 		printf("Enter a new command...\n"
 			"Type '0' in the end of the command\n");
 		c = getchar();
@@ -19,13 +29,81 @@ int main()
 
 		while((c != '0') && (count < MAXBUFFERSIZE))
 		{
-			buffer[count++] = c;
+			buf[count++] = c;
 			c = getchar();
 
 		}
 		
-		buffer[count] = '\0';
-		printf("The command u entered was:%s\n", buffer);
-		exit_flag = 0;	
+		buf[count] = '\0';
+		printf("The command u entered was:%s\n", buf);
+		parsing_command(buf);
+		//exit_flag = 0;
+		free(buf);	
 	}
+}
+
+void parsing_command(char *command)
+{
+	printf("Parsing=%s\n", command);
+	char com[MAXBUFFERSIZE];
+	
+	int count=0;
+	
+	/* Вырезаем первое слово до пробела */
+	while(*command != ' ')
+	{
+		com[count++] = *command;
+		command++;
+	}
+	command++;
+	com[count]='\0';
+	//printf("First word=%s\n", com);
+	
+	char *start = command; //Сохраним указатель на начале параметров
+	
+	/* Посчитаем количество параметров */
+	int w = 0; //Количество пробелов
+	while(*command != '\0')
+	{
+		if(*command == ' ') w++;
+		command++;
+	}w++;	
+	
+	char *param[w]; //Массив для параметров
+	int l = 0;
+	int p = 0;
+	printf("Number of words = %d\n", w);
+
+	for(int i=0; i<w; i++)
+	{
+		param[i] = start;
+		do
+		{
+			start++;
+			if(*start == '\0') goto end;
+		}while (*start != ' ');	
+		
+		*start = '\0';
+		start++;
+	}
+end:
+	/* Вызываем нужную функцию */	
+	if(strcmp(com, "add")==0)
+	{
+		add(param, w);
+	}
+	else
+	{
+		printf("Error: No command found.\n");
+	}
+	
+
+}
+
+void add(char *p[], int number)
+{
+	printf("Add function.\n");
+	printf("Parametrs are:\n");
+	for(int i=0; i<number; i++)
+		printf("-%s\n", p[i]);
 }
